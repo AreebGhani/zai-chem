d3eb4fad64aa4bb62c04314a91aea5b09f8c1e3d
Areeb Ghani <91328326+AreebGhani@users.noreply.github.com>
2022-12-28 00:48:48 +0500

    27/12/2022

--- src/sections/VideoPlay/VideoPlay.jsx
+++ src/sections/VideoPlay/VideoPlay.jsx
@@ -6,7 +6,7 @@ export default function VideoPlay() {
         <section className="video-section-two">
             <div className="auto-container">
                 <div className="content-box">
-                    <a href="#" className="play-now"><i className="icon fa fa-play" aria-hidden="true"></i><span className="ripple"></span></a>
+                    <a href="#video" className="play-now"><i className="icon fa fa-play" aria-hidden="true"></i><span className="ripple"></span></a>
                     <h4>Industris provides the best service <br />for sustainable progress</h4>
                 </div>
             </div>
